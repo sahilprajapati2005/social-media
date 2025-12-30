1423c629e6b5497e1e00aafd7f17dfebc9222c66
sahilprajapati2005 <sahilprajapati0105@gmail.com>
2025-12-30 18:27:52 +0530

    export app

--- server/server.js
+++ server/server.js
@@ -111,3 +111,5 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+
+module.exports = app;
